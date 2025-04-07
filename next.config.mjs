1381f9afe2e4457daa50ca6b3cb810b6bc87ce24
Lo <lrem@live.fr>
2025-04-07 12:16:37 +0200

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/:path*', // ou '/api/:path*' si tu veux limiter aux API routes
+          headers: [
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: '*' // ou 'https://www.appliq.fr' si tu veux restreindre
+            },
+            {
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET, POST, PUT, DELETE, OPTIONS'
+            },
+            {
+              key: 'Access-Control-Allow-Headers',
+              value: 'Content-Type, Authorization, rsc'
+            },
+          ]
+        }
+      ];
+    },
+  };
+  
+  export default nextConfig;
